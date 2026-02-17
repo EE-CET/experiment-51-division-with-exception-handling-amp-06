d9fc62a94b32102f95bfd2ca191bc568d589c21c
amrita-m-pillai <amritampillai06@gmail.com>
2026-02-17 17:37:24 +0530

    first commit

--- src/DivisionHandling.java
+++ src/DivisionHandling.java
@@ -2,10 +2,23 @@ import java.util.Scanner;
 
 public class DivisionHandling {
     
-        // TODO: Read two integers a and b
-        
-        // TODO: Enclose the division in a try block
-        
-        // TODO: Catch ArithmeticException
-        
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        try {
+            int z = a / b;
+            System.out.println( z);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Divide by zero error");
+        }
+    }
 }
+
+        
+
